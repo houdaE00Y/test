005ceb3a54d493ef1dfeecda2646be1b37f1f34d
dama <dama@localhost>
2023-04-29 15:34:57 +0200

    Send-Receive onto

--- src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ReceiveOntologiesBehaviour.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ReceiveOntologiesBehaviour.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import org.apache.jena.rdf.model.Model;
 
+import dataStructures.serializableGraph.SerializableSimpleGraph;
 import eu.su.mas.dedale.mas.AbstractDedaleAgent;
 import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation.MapAttribute;
 import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 public class ReceiveOntologiesBehaviour extends SimpleBehaviour {
 
@@ -14,11 +19,24 @@ public class ReceiveOntologiesBehaviour extends SimpleBehaviour {
 	MapaModel model;
 	
 	public ReceiveOntologiesBehaviour(final AbstractDedaleAgent myAgent, MapaModel model, List<String> agents) {
+		this.agents = agents;
+		this.model = model;
 	}
 	
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
+
+		MessageTemplate msgTemplate = MessageTemplate.and(
+                MessageTemplate.MatchProtocol("SHARE-ONTO"),
+                MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+        ACLMessage msgReceived = this.myAgent.receive(msgTemplate);
+        if (msgReceived != null) {
+            try {
+                model.importOntology((String) msgReceived.getContentObject());
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+        }
 
 	}
 
--- src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/SendOntologiesBehaviour.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/SendOntologiesBehaviour.java
@@ -1,12 +1,17 @@
 package eu.su.mas.dedaleEtu.mas.behaviours;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.jena.rdf.model.Model;
 
+import dataStructures.serializableGraph.SerializableSimpleGraph;
 import eu.su.mas.dedale.mas.AbstractDedaleAgent;
 import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation.MapAttribute;
+import jade.core.AID;
 import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class SendOntologiesBehaviour extends SimpleBehaviour {
 
@@ -15,12 +20,24 @@ public class SendOntologiesBehaviour extends SimpleBehaviour {
 	MapaModel model;
 	
 	public SendOntologiesBehaviour(final AbstractDedaleAgent myAgent, MapaModel model, List<String> agents) {
+		this.agents = agents;
+		this.model = model;
 	}
 	
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        msg.setProtocol("SHARE-ONTO");
+        msg.setSender(this.myAgent.getAID());
+        for (String agentName : agents) {
+            msg.addReceiver(new AID(agentName, AID.ISLOCALNAME));
+        }
+        try {
+            msg.setContentObject(model.getOntology());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ((AbstractDedaleAgent) this.myAgent).sendMessage(msg);
 	}
 
 	@Override
